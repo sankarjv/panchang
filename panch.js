325c19f1babdd4c21be20f4a048da1a5768b7ffe
sankarjv <sankarjv@users.noreply.github.com>
2025-04-27 15:03:24 +0530

    Update panch.js

--- panch.js
+++ panch.js
@@ -422,7 +422,7 @@ if (itit == (n1 + 1)) s_t.end=calData(jdt + (tzone - dt)/24);
 console.log("jd "+jd+" n1 "+n1+" len "+len);
 console.log("jdt "+jdt+" knv "+knv+" itit "+itit);
 console.log("st "+s_t.start+" se "+s_t.end);
-console.log("a "+a+" asp "+asp+" asp1 "+asp1," skor "+skor+" aspect "+aspect);
+console.log("a "+a+" asp1 "+asp1," skor "+skor+" aspect "+aspect);
  return s_t;
 }
 
